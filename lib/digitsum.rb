1f77476f1a53b74c27f3905983d4804f929bb914
manikimpinge <manik.impinge@gmail.com>
2018-08-20 15:40:20 +0530

    Exception handling

--- lib/digitsum.rb
+++ lib/digitsum.rb
@@ -5,7 +5,18 @@ module Digitsum
  class Math
 
 	 def self.summarize digit
-	 	digit.to_s.chars.map(&:to_i).reduce(:+)
+	 
+	 	 begin   
+	 	   if digit.is_a? Integer
+	 	  	puts digit.to_s.chars.map(&:to_i).reduce(:+)
+	 	   else
+	 	  	puts "TypeError: argument is not an integer"
+	 	   end
+
+		 rescue StandardError => msg 
+		   puts msg  
+		 end  
+
 	 end
 
  end
