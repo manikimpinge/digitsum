49575aa7fb5d43a5c798d083115bbcc788282ef3
manikimpinge <manik.impinge@gmail.com>
2018-08-20 15:00:06 +0530

    Updated Gem

--- lib/digitsum.rb
+++ lib/digitsum.rb
@@ -2,8 +2,12 @@ require "digitsum/version"
 
 module Digitsum
  
- def summarize digit
- 	digit.to_s.chars.map(&:to_i).reduce(:+)
+ class Math
+
+	 def self.summarize digit
+	 	digit.to_s.chars.map(&:to_i).reduce(:+)
+	 end
+
  end
 
 end
